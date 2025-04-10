13dac1724e3c491c70b910dfecb35f99d07fa306
iMeeTake <arceniy_radaev@mail.ru>
2025-04-10 12:31:10 +0500

    Initial Commit

---
+++ TAPI 1.21.5 Fabric/src/main/java/com/imeetake/tapi/item/TItemBuilder.java
@@ -0,0 +1,34 @@
+package com.imeetake.tapi.item;
+
+import com.imeetake.tapi.registry.TRegistry;
+import net.minecraft.item.Item;
+
+public class TItemBuilder {
+    private final String id;
+    private final Item.Settings settings;
+
+    private TItemBuilder(String id) {
+        this.id = id;
+        this.settings = new Item.Settings();
+    }
+
+    public static TItemBuilder create(String id) {
+        return new TItemBuilder(id);
+    }
+
+    public TItemBuilder maxCount(int count) {
+        settings.maxCount(count);
+        return this;
+    }
+
+    public TItemBuilder maxDamage(int damage) {
+        settings.maxDamage(damage);
+        return this;
+    }
+
+
+    public Item build() {
+        Item item = new Item(settings);
+        return TRegistry.registerItem(id, item);
+    }
+}
---
+++ TAPI 1.21.5 Fabric/src/main/java/com/imeetake/tapi/item/TToolBuilder.java
@@ -0,0 +1,84 @@
+package com.imeetake.tapi.item;
+
+import com.imeetake.tapi.registry.TRegistry;
+import net.minecraft.item.Item;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+public class TToolBuilder {
+    private final String id;
+    private final ToolMaterial material;
+    private final Item.Settings settings;
+
+    private int damage = 1;
+    private float attackSpeed = -2.8f;
+    private ToolType type;
+
+    private TToolBuilder(String id, ToolMaterial material, ToolType type) {
+        this.id = id;
+        this.material = material;
+        this.type = type;
+        this.settings = new Item.Settings();
+    }
+
+    public static TToolBuilder pickaxe(String id, ToolMaterial material) {
+        return new TToolBuilder(id, material, ToolType.PICKAXE);
+    }
+
+    public static TToolBuilder axe(String id, ToolMaterial material) {
+        return new TToolBuilder(id, material, ToolType.AXE);
+    }
+
+    public static TToolBuilder shovel(String id, ToolMaterial material) {
+        return new TToolBuilder(id, material, ToolType.SHOVEL);
+    }
+
+    public static TToolBuilder hoe(String id, ToolMaterial material) {
+        return new TToolBuilder(id, material, ToolType.HOE);
+    }
+
+    public TToolBuilder damage(int damage) {
+        this.damage = damage;
+        return this;
+    }
+
+    public TToolBuilder attackSpeed(float speed) {
+        this.attackSpeed = speed;
+        return this;
+    }
+
+    public TToolBuilder maxCount(int count) {
+        settings.maxCount(count);
+        return this;
+    }
+
+    public TToolBuilder maxDamage(int damage) {
+        settings.maxDamage(damage);
+        return this;
+    }
+
+    public Item build() {
+        Identifier identifier = TRegistry.getItemId(id);
+
+        // Применяем соответствующий метод для инструмента
+        switch (type) {
+            case PICKAXE -> settings.pickaxe(material, damage, attackSpeed);
+            case AXE -> settings.axe(material, damage, attackSpeed);
+            case SHOVEL -> settings.shovel(material, damage, attackSpeed);
+            case HOE -> settings.hoe(material, damage, attackSpeed);
+            default -> throw new IllegalStateException("Unknown tool type: " + type);
+        }
+
+        // Обязательный ключ для корректной регистрации атрибутов
+        settings.registryKey(RegistryKey.of(RegistryKeys.ITEM, identifier));
+
+        Item item = new Item(settings);
+        return TRegistry.registerItem(id, item);
+    }
+
+    private enum ToolType {
+        PICKAXE, AXE, SHOVEL, HOE
+    }
+}
